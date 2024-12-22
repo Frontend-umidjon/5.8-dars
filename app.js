444aab26138a09d12c48876d736d604b04e92a42
Frontend-Umidjon <umidjonkengashev6@gamil.com>
2024-12-22 21:47:23 +0500

    uy ishi

--- app.js
+++ app.js
@@ -1,89 +1,91 @@
-const USERS = JSON.parse(localStorage.getItem('users')) || [
-    {
-        fname: 'Json',
-        lname: 'Watson',
-        role: 'CEO & Founder',
-        university: 'Oxford',
-        gender: 'female'
-    },
-    {
-        fname: 'Jon',
-        lname: 'Watson',
-        role: 'CEO & Founder',
-        university: 'Oxford',
-        gender: 'male'
-    },
-    {
-        fname: 'Nick',
-        lname: 'Watson',
-        role: 'CEO & Founder',
-        university: 'Oxford',
-        gender: 'male'
-    },
-    {
-        fname: 'Joy',
-        lname: 'Watson',
-        role: 'CEO & Founder',
-        university: 'Oxford',
-        gender: 'female'
-    }
-];
+const BASE_URL = 'http://localhost:3000';
+const team = document.querySelector('.team')
+ const addNew = document.querySelector('.add__new')
+ const modal = document.querySelector('.modal')
+ const modalForm = document.querySelector('.modal__form')
+ const fname = document.querySelector('#fname')
+ const lname = document.querySelector('#lname')
+ const role = document.querySelector('#role')
+ const university = document.querySelector('#university')
+ const gender = document.querySelector('#gender')
 
-const teamEl = document.querySelector('.team');
-const modalEl = document.querySelector('.modal');
-const modalFormEl = document.querySelector('.modal__form');
-function createUserCard(user) {
-    const cardEl = document.createElement('div');
-    cardEl.classList.add('team__card');
-    cardEl.innerHTML = `
-        <img src="${user.gender === 'male' ? './assets/man.png' : './assets/woman.png'}" class="team__card-image">
-        <h2 class="team__card-name">${user.fname} ${user.lname}</h2>
-        <p class="team__card-role">${user.role}</p>
-        <p class="team__card-university">${user.university}</p>
-        <div class="team__card-social">
-            <i class="fab fa-instagram"></i>
-            <i class="fab fa-twitter"></i>
-            <i class="fab fa-facebook-f"></i>
-        </div>
-        <button class="team__card-button">Contact</button>
-    `;
+async function fetchData() {
+    const response = await fetch(`${BASE_URL}/users`)
+    
+    response 
+        .json()
+        .then(res => {
+            createUserCards(res)
+        })
+        .catch(err => console.log(err))
+}
 
-    teamEl.appendChild(cardEl);
+window.onload = () => {
+    fetchData()
 }
-function saveToLocalStorage() {
-    localStorage.setItem('users', JSON.stringify(USERS));
-}
-window.addEventListener('load', () => {
-    USERS.forEach(user => {
-        createUserCard(user);
-    });
+
+addNew.addEventListener('click', () => {
+    modal.style.display = 'flex'
+})
+addNew.addEventListener('blur', () => {
+    modal.style.display = 'none'
 })
 
-const addNewEl = document.querySelector('.add__new');
-addNewEl.addEventListener('click', () => {
-    modalEl.style.display = 'flex';
-});
-modalFormEl.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const fname = document.querySelector('#fname').value.trim();
-    const lname = document.querySelector('#lname').value.trim();
-    const role = document.querySelector('#role').value.trim();
-    const university = document.querySelector('#university').value.trim();
-    const gender = document.querySelector('#gender').value.trim().toLowerCase();
-    if (!fname || !lname || !role || !university || (gender !== 'male' && gender !== 'female')) {
-        alert('Please fill in all fields correctly. Gender should be "male" or "female".');
-        return;
+function createUserCards(users) {
+    users.forEach(user => {
+        const card = document.createElement('div')
+        card.classList.add('team__card')
+        card.innerHTML = `
+            <button name="delete" class="team__card-delete" data-id="${user.id}">X</button>
+            <img src="${user.gender === 'male' ? './assets/man.png' : './assets/woman.png'}" class="team__card-image">
+            <h2 class="team__card-name">${user.fname} ${user.lname}</h2>
+            <p class="team__card-role">${user.role}</p>
+            <p class="team__card-university">${user.university}</p>
+            <div class="team__card-social">
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-instagram"></i>
+            </div>
+        `
+        team.appendChild(card)
+    })
+}
+
+modalForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let newUser = {
+        fname: modalForm.fname.value,
+        lname: modalForm.lname.value,
+        role: modalForm.role.value,
+        university: modalForm.university.value,
+        gender: modalForm.gender.value,
     }
-    const newUser = {
-        fname,
-        lname,
-        role,
-        university,
-        gender
-    };
-    USERS.push(newUser);
-    saveToLocalStorage();// Ustoz agar ko'rayotgan bo'lsangiz shuni o'tgan safargi vazifani shu funksiya bilan hal qildim kattakon raxmat.
-    createUserCard(newUser);
-    modalEl.style.display = 'none';
-    modalFormEl.reset();
-});
+    fetch(`${BASE_URL}/users`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+    })
+        .then(res => res.json())
+        .then(res => {
+            createUserCards([res])
+            modalForm.innerHTML= null
+            modal.style.display = 'none'
+        })
+        .catch(err => console.log(err))
+    }
+)
+
+team.addEventListener('click', (e) => {
+    if (e.target.name === ( 'delete')) {
+       let id = e.target.dataset.id
+       fetch(`${BASE_URL}/users/${id}`, {
+           method: 'DELETE',
+           headers: {
+               'Content-Type': 'application/json'
+           }
+       })
+       }
+    }
+)
